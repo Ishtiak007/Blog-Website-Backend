1d8c9d3b756f93d676b567a5ce820a4c98e26836
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-10 00:45:27 +0600

    add update blog service logic with ownership check

--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -32,7 +32,45 @@ const getAllBlogsFromDB = async (query: Record<string, unknown>) => {
   return result;
 };
 
+// Update blog from Database
+const updateBlogIntoDB = async (
+  id: string,
+  userEmail: string,
+  payload: Partial<TBlog>,
+) => {
+  // check user is exists
+  const user = await User.isUserExists(userEmail);
+
+  if (!user) {
+    throw new AppError(403, 'User not found! You cannot update the blog.');
+  }
+
+  // check blog is exists
+  const blog = await Blog.findById(id);
+
+  if (!blog) {
+    throw new AppError(404, 'Blog not found! You cannot update it.');
+  }
+
+  // check the owner
+
+  if (blog.author.toString() !== user._id.toString()) {
+    throw new AppError(
+      403,
+      'You are not the owner of this blog and cannot update it.',
+    );
+  }
+
+  const result = await Blog.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return result;
+};
+
 export const BlogServices = {
   createBlogIntoDB,
   getAllBlogsFromDB,
+  updateBlogIntoDB,
 };

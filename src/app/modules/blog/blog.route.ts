36634042c2e5d7c4cb47067f45e57f4aee915f0b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-09 21:44:53 +0600

    add blog routes with auth and validation

---
+++ src/app/modules/blog/blog.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { auth } from '../../middlewares/auth';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { blogValidationSchema } from './blog.validation';
+import { USER_ROLE } from '../user/user.constant';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  auth(USER_ROLE.user),
+  validateRequest(blogValidationSchema.createBlogValidationSchema),
+  BlogControllers.createBlog,
+);
+
+router.get('/', BlogControllers.getAllBlogs);
+
+export const BlogRoutes = router;

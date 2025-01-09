be4122bc5dddc6a6f12c7e971fe3f35d6209e8b2
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-09 09:39:53 +0600

    Configure environment variables using dotenv for application settings

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,9 +1,13 @@
-import path from 'path';
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join((process.cwd(), '.env')) });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
+  node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  databaseURI: process.env.DATABASE_URL,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  access_token_expires_in: process.env.ACCESS_TOKEN_EXPIRES_IN,
 };

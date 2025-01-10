5cf9da2365259dc441930500a4650e9c645a93e8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-10 11:04:14 +0600

    add login route with validation middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
-import { userValidationSchema } from './user.validation';
+import { loginValidationSchema, userValidationSchema } from './user.validation';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
+// user register
 router.post(
   '/register',
   validateRequest(userValidationSchema.registerUserValidationSchema),
   UserControllers.registerUser,
+);
+
+// User login
+router.post(
+  '/login',
+  validateRequest(loginValidationSchema),
+  UserControllers.loginUser,
 );
 
 export const UserRoutes = router;

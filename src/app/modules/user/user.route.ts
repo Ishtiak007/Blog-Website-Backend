1b586ef65c58a7999e233b0de02239442c8957c1
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-10 11:11:55 +0600

    add admin-only delete blog route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { loginValidationSchema, userValidationSchema } from './user.validation';
 import { UserControllers } from './user.controller';
+import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
@@ -18,5 +20,8 @@ router.post(
   validateRequest(loginValidationSchema),
   UserControllers.loginUser,
 );
+
+// Delete user
+router.delete('/blogs/:id', auth(USER_ROLE.admin), UserControllers.deleteBlog);
 
 export const UserRoutes = router;

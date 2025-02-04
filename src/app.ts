e36ab0f65aa9655bd375201a40df6dfdf0a8861b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-04 13:09:23 +0600

    app.ts file updated

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Application, Request, Response } from 'express';
-const app: Application = express();
+import express from 'express';
 import cors from 'cors';
+import { router } from './app/routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import { notFound } from './app/middlewares/notFound';
 
-// Parsers
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(
-    'Hello developers, I am Ishtiak From Rangpur, I am developing Blog server assignment 🦄!',
-  );
+app.get('/', (req, res) => {
+  res.send('Hello, Server is running.');
 });
+app.use('/api', router);
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
